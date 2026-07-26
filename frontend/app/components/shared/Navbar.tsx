f9e0743e5831e1d595ab4edc248b998b4d352635
Farhan Hassan Jabil <farhanhasan295@gmail.com>
2026-07-26 20:46:00 +0600

    fixing landingPage

--- frontend/app/components/shared/Navbar.tsx
+++ frontend/app/components/shared/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
             <Image
               src={logo}
               alt="Work Stream Logo"
-              className="h-[50px] lg:h-[100px] w-[100px] lg:w-[150px] rounded-xl"
+              className="h-[40px] md:h-[70px] 3xl:h-[100px] w-[80px] md:w-[100px] 3xl:w-[150px] rounded-xl"
             />
           </ScrollLink>
 
@@ -64,7 +64,7 @@ const Navbar = () => {
                   to={item.to}
                   smooth={true}
                   duration={500}
-                  className="nav-link cursor-pointer hover:text-[#9191c4] focus:outline-none transition-colors duration-500 ease-in-out"
+                  className="nav-link cursor-pointer hover:text-[#9191c4] focus:outline-none transition-colors duration-500 ease-in-out "
                   activeClass="active"
                 >
                   {item.label}
@@ -74,13 +74,13 @@ const Navbar = () => {
           </ul>
 
           <div className="hidden md:flex">
-            <Link href="/signInUp">
+            <Link href="/auth/login">
               <button
                 type="button"
-                className="relative overflow-hidden text-white bg-gradient-to-r from-green-500 to-blue-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 transition-transform duration-500 ease-in-out group"
+                className="relative overflow-hidden text-white bg-gradient-to-r from-green-500 to-blue-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 transition-transform duration-500 ease-in-out group cursor-pointer"
               >
                 <span className="absolute inset-0 bg-gradient-to-r from-green-600 to-blue-600 rounded-lg transform scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out"></span>
-                <span className="relative z-10 flex items-center justify-center">
+                <span className="relative z-10 flex items-center justify-center ">
                   Login
                 </span>
               </button>
